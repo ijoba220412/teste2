d79c05a8e0bd4404f36871df441b7d20951453a6
ijoba220412 <95876908+ijoba220412@users.noreply.github.com>
2026-05-30 13:33:07 -0300

    Simplify next.config.mjs configuration
    Removed unused imports and adjusted configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const nextConfig = {
-  experimental: {
-    optimizePackageImports: ['lucide-react'],
+  images: {
+    unoptimized: true, // Importante para imagens locais no Firebase/Vercel
   },
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(__dirname, 'src'),
-    };
-    return config;
-  },
-}
-
+};
 export default nextConfig;
